bb1b5059288945ecd6d13dbd1f07c7a04c809cb5
hassan nawaz <hassancent@gmail.com>
2024-02-13 18:00:17 +0500

    Update setup-php-site.sh script to improve user prompts and display site URL

--- setup-php-site.sh
+++ setup-php-site.sh
@@ -3,13 +3,13 @@
 # ask user to enter php version to use
 echo "Following are php versions installed on your server:"
 ls /etc/php | grep -E '^[0-9]'
-read -p "Enter the PHP version you want to install (e.g., 8.1): " php_version
+read -p "Enter the PHP version you want to use (e.g., 8.1): " php_version
 
 # Ask the user for the site name
-read -p "Enter the nginx site name. This will create folder here : /var/www/site_name. Best practice to keep it same as domain : " site_name
+read -p "Enter the nginx site name. This will create folder here : /var/www/site_name. Best practice to keep it same as domain : \n" site_name
 
 # Ask the user for the server name
-read -p "Enter the nginx server name (domains separated by space) i.e, 'byteremix.com www.byteremix.com' (leave empty to use site name '$site_name'): " server_name
+read -p "Enter the nginx server name (domains separated by space) i.e, 'byteremix.com www.byteremix.com' (leave empty to use site name '$site_name'): \n" server_name
 
 # If server_name is empty, set it to the same value as site_name
 if [ -z "$server_name" ]; then
@@ -17,7 +17,8 @@ if [ -z "$server_name" ]; then
 fi
 
 # Ask the user if they want to enable SSL
-read -p "Do you want to enable SSL (listen to 443 port)? (y/n): " enable_ssl
+read -p "Do you want to enable SSL (listen to 443 port)? (Y/n): " enable_ssl
+enable_ssl=${enable_ssl:-y}
 
 # If the user wants to enable SSL, remove the comment from the SSL configuration lines
 if [ "$enable_ssl" = "y" ]; then
@@ -55,4 +56,10 @@ if [ ! -d "/var/www/$site_name" ]; then
 fi
 sudo cp ./templates/new_php_site.php /var/www/$site_name/index.php
 
-echo "Nginx site for $site_name has been created and enabled."
+echo "\n\nNginx site for $site_name has been created and enabled."
+first_domain=$(echo "$server_name" | cut -d' ' -f1)
+if [ "$enable_ssl" = "y" ]; then
+    echo "You can access it at https://$first_domain"
+else
+    echo "You can access it at http://$first_domain"
+fi
