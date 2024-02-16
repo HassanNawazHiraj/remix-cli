9cd2a7da1b2a432ba4b21fd019c62b80bcebfcbf
hassan nawaz <hassancent@gmail.com>
2024-02-16 15:11:03 +0500

    Add warning message and prompt for user confirmation before continuing script execution

--- setup-repo-for-site.sh
+++ setup-repo-for-site.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 DIR="$(dirname "$0")"
 
+echo -e "WARNING: This script will clone a git repository into /var/www/site_name. user will be prompted for site_name \n"
+echo -e "If this directory is not empty, then its content will be lost. \n"
+
+# ask user if they want to continue
+read -p "Do you want to continue? (Y/n): " continue_script
+continue_script=${continue_script:-y}
+
 # ask user if they want to generate ssh key
 echo -e "Please make sure you have a git repository created and the ssh key added to your git account. \n"
 echo -e "You can generate new key for each git respository and add it to deploy keys of that git \n"
@@ -26,6 +33,9 @@ done
 
 # Ask user for the site name
 read -p "Enter the site name from /var/www/site_name (e.g., example.com): " site_name
+
+# clear /var/www/site_name folder
+sudo rm -rf /var/www/$site_name
 
 # clone the repository into /var/www/$site_name
 sudo git clone $git_repo /var/www/$site_name
