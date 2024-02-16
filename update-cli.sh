d2ed3e72fed36110daa0257e6f40c7ea15c33bca
hassan nawaz <hassancent@gmail.com>
2024-02-16 13:40:11 +0500

    Update CLI script and improve output message

--- update-cli.sh
+++ update-cli.sh
@@ -3,12 +3,12 @@
 echo "Updating CLI..."
 
 # Fetch the remote repository
-git fetch
+git fetch > /dev/null 2>&1
 
 # Reset the local repository to match the remote repository
-git reset --hard origin/master
+git reset --hard origin/master > /dev/null 2>&1
 
 # Make the script executable
 chmod +x *.sh
 
-echo -e "\n\n CLI updated."
+echo -e "\nCLI updated."
