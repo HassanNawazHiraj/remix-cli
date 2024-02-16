b8992b5691d773acf762e925ef138ae05d78b77a
hassan nawaz <hassancent@gmail.com>
2024-02-16 15:54:47 +0500

    Update CLI script to check for updates before updating

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Updating CLI..."
+echo "Checking for updates..."
 
 # Change to the directory of the script
 cd "$(dirname "$0")"
@@ -16,13 +16,15 @@ after_update=$(git rev-parse origin/master)
 
 # If the commit hashes are different, reset the local repository and make the script executable
 if [ "$before_update" != "$after_update" ]; then
+    echo -e "Updating CLI..."
+
     # Reset the local repository to match the remote repository
     git reset --hard origin/master > /dev/null 2>&1
 
     # Make the script executable
     chmod +x *.sh
 
-    echo -e "\nCLI updated."
+    echo -e "CLI updated."
 else
-    echo -e "\nNo updates available."
+    echo -e "No updates available."
 fi
