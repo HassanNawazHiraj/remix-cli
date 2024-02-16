ff936280f6cf5c6ebdb1491789da62d7fa21936c
hassan nawaz <hassancent@gmail.com>
2024-02-16 13:36:27 +0500

    bug fixes in setup cli

--- setup-cli.sh
+++ setup-cli.sh
@@ -12,12 +12,12 @@ if [[ -z $input_filename ]]; then
 fi
 
 # Replace {{SCRIPT_PATH}} with the actual script path
-sed "s/{{SCRIPT_PATH}}/$(pwd)/g" ./templates/remix > "$input_filename"
+sed "s#{{SCRIPT_PATH}}#$(pwd)#g" ./templates/remix > "$input_filename"
 
 # Make the file executable
 chmod +x "$input_filename"
 
 # Copy the file to /usr/local/bin
-mv "$input_filename" /usr/local/bin
+sudo mv "$input_filename" /usr/local/bin
 
 echo "Cli is setup to use $input_filename now! You can run the cli using the command '$input_filename <command>'."
