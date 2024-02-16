9cc8e2f43e4ff1b2f40ac7947b0dfc41d01a84c0
hassan nawaz <hassancent@gmail.com>
2024-02-16 16:30:03 +0500

    Add apt-get update command to install-laravel-php-extensions.sh script

--- install-laravel-php-extensions.sh
+++ install-laravel-php-extensions.sh
@@ -10,5 +10,7 @@ update_cli_php_version $php_version
 # List of required PHP packages
 sudo apt install openssl php-bcmath php-curl php-json php-mbstring php-mysql php-tokenizer php-xml php-zip
 
+sudo apt-get update
+
 # Restart the PHP service to apply changes
 sudo service php${php_version}-fpm restart
