8dd13b30952308f4340ecbb2dabda562d93cdc06
hassan nawaz <hassancent@gmail.com>
2024-02-16 15:51:07 +0500

    Fix SSH agent startup and key addition

--- start-ssh-server.sh
+++ start-ssh-server.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-if ! pgrep -q ssh-agent; then
+if ! pgrep -x "ssh-agent" > /dev/null; then
     eval "$(ssh-agent -s)"
 fi
 
 # add all the keys to the agent
 for key in ~/.ssh/*; do
+    chmod 600 $key > /dev/null
     ssh-add $key > /dev/null
 done
+
+echo "SSH agent started and all keys added."
