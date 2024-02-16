c109e84f85ed3ae19084d42633781a401a566fbc
hassan nawaz <hassancent@gmail.com>
2024-02-16 12:52:53 +0500

    Update CLI script to fetch and reset from origin/master

--- update-cli.sh
+++ update-cli.sh
@@ -3,10 +3,10 @@
 echo "Updating CLI..."
 
 # Fetch the remote repository
-git fetch origin
+git fetch
 
 # Reset the local repository to match the remote repository
-git reset --hard origin/main
+git reset --hard origin/master
 
 # Make the script executable
 chmod +x *.sh
