f046caf4055b26d770b93d6fc41a2c30b67203a1
hassan nawaz <hassancent@gmail.com>
2024-02-16 17:48:11 +0500

    Add script to edit nginx config and update nginx template

---
+++ edit-nginx-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# get path to current directory
+project_name=$(basename "$(pwd)")
+
+# see if a nginx config exist with the same name as the project
+if [ ! -f /etc/nginx/sites-available/$project_name ]; then
+    echo "Not in project folder"
+    # ask user to enter site name
+    read -p "Enter the nginx site name. " site_name
+    else
+    site_name=$project_name
+fi
+
+
+cat /etc/nginx/sites-available/$site_name
